8b0c6efc4589bf4d450e69627ad7d6075b5e43ba
Bar Yaron <bary@atera.com>
2021-12-25 01:36:20 +0200

    Add Opal 2

---
+++ Test2.java
@@ -0,0 +1,92 @@
+class A {
+
+    private int _x;
+
+    public A(int x)  {
+
+        _x = x;
+
+    }
+
+    public int getX()   {
+
+        return _x;
+
+    }
+
+    public int doubleX()  {
+
+        return 2*getX();
+
+    }
+
+    public int tripleX()   {
+
+        return 3*_x;
+
+    }    
+
+    public int subXhelper()  {
+
+        return _x-1;
+
+    }    
+
+    public int subX()  {
+
+        return subXhelper();
+
+    }    
+
+}
+
+//--------------------------------------------------//
+
+class B extends A {
+
+    private int _x;
+
+    public B(int xA, int xB)  {
+
+        super(xA);
+
+        _x = xB;
+
+    }
+
+    public int getX()  {
+
+        return _x;
+
+    }
+
+    public int superX()  {
+
+        return super.getX();
+
+    } 
+
+    public int tenTimesX()  {
+
+        return 10*_x;
+
+    }
+
+    public int subXhelper()  {
+
+        return _x-2;
+
+    }    
+
+}
+
+public class Test2{
+    public static void main(String[]args)
+    {
+        A a = new A(1);
+
+        A b = new B(2, 22);
+
+        System.out.println(b.doubleX());
+    }
+}
